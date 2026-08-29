79356aaad62e1a346133921820da4f7bd3443a7f
tcook4409 <tcook4409@gmail.com>
2026-08-29 03:06:26 -0400

    Create sw.js

---
+++ order-prep/sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'order-prep-pwa-v1';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
